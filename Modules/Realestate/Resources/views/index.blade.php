6cb4f05b73999fae34ffd77d2c03a2e520708bfd
shakil <belal8804505973s@gmail.com>
2022-10-25 17:57:18 +0530

    Asseets Add

--- Modules/Realestate/Resources/views/index.blade.php
+++ Modules/Realestate/Resources/views/index.blade.php
@@ -1,9 +1,12 @@
 @extends('realestate::layouts.master')
+@section('title')
+<title>Realestate List</title>
 
+@endsection
 @section('content')
-    <h1>Hello World</h1>
+<h1>Hello World</h1>
 
-    <p>
-        This view is loaded from module: {!! config('realestate.name') !!}
-    </p>
+<p>
+    This view is loaded from module: {!! config('realestate.name') !!}
+</p>
 @endsection
